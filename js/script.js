cfa2398cd3bf55a7da06541837a39668c3682e70
mariahMurdock <mariahmurdock1227@gmail.com>
2020-10-12 11:45:53 -0700

    testing

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 function buildDynamicNav() {
-  const navbarSections = ['','About', 'Dogs', 'Cats', 'Exotic', 'Contact'];
-  const navbarHrefs = ['index.html', 'dogs.html', 'cats.html', 'exotic.html', 'contact.html'];
+  const navbarSections = ['', 'About', 'Dogs', 'Cats', 'Exotic', 'Contact'];
+  const navbarHrefs = ['', '#about', '#dogs', '#cats', '#exotic', '#contact'];
   const navBarUl = document.querySelector('#navbar__list');
   for (let i = 0; i < 6; i++) {
     const navLi = document.createElement('li');
     navBarUl.appendChild(navLi);
     const btn = document.createElement('button');
     navLi.appendChild(btn);
+    btn.setAttribute('class', 'button');
     const anchorElements = document.createElement('a');
     btn.appendChild(anchorElements);
     anchorElements.setAttribute('href', navbarHrefs[i]);
@@ -17,7 +18,9 @@ console.log(buildDynamicNav());
 
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {
+  myFunction()
+};
 
 // Get the navbar
 const navbar = document.getElementById("navbar__list");
@@ -32,4 +35,21 @@ function myFunction() {
   } else {
     navbar.classList.remove("sticky");
   }
+}
+
+// add highlight to navbar
+let allElements = document.getElementsByClassName('button');
+for (let i = 0; i < allElements.length; i++) {
+  // add event listener while iterating through btn class elements
+  allElements[i].addEventListener('click', function() {
+    // if any, remove all active classes
+    let currentElements = document.getElementsByClassName('active');
+    // grab active class
+    if (currentElements.length > 0) {
+      // replace active class with empty string
+      currentElements[0].className = currentElements[0].className.replace(' active', '');
+    }
+    // replace className with active
+    this.className += ' active';
+  });
 }
